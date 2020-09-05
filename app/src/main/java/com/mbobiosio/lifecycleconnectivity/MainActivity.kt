e80f98a86479f324f2ce2c61634291814b12fa86
Mbuodile Obiosio <cazewonder@gmail.com>
2020-09-05 20:21:30 +0100

    minor changes

--- app/src/main/java/com/mbobiosio/lifecycleconnectivity/MainActivity.kt
+++ app/src/main/java/com/mbobiosio/lifecycleconnectivity/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,4 +18,5 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
+
 }
